4556fd728ba1db9527a9a773127683e9571f1155
Oleg <sleite4r@gmail.com>
2025-05-16 13:22:13 +0200

    style in css

--- src/components/QouteCard/QuoteCard.jsx
+++ src/components/QouteCard/QuoteCard.jsx
@@ -1,16 +1,19 @@
 //QuoteCard.jsx
 
 import React from "react";
+import styles from "./QuoteCard.module.css";
 import { Link } from "react-router-dom";
 
 const QuoteCard = ({ quote }) => {
   if (!quote) return <p>No quote data</p>;
-  
+
   return (
-    <div>
-      <p>{quote.quote}</p>
-      <p>{quote.author}</p>
-      <Link to={`/quote/${quote.id}`}>View Details</Link>
+    <div className={styles.ListItem}>
+      <p className={styles.quoteText}>{quote.quote}</p>
+      <p className={styles.quoteAuthor}>{quote.author}</p>
+      <Link className={styles.quoteLink} to={`/quote/${quote.id}`}>
+        View Details
+      </Link>
     </div>
   );
 };
--- src/components/QuoteList/QuoteList.jsx
+++ src/components/QuoteList/QuoteList.jsx
@@ -1,10 +1,11 @@
 //QuoteList.jsx
 import React from "react";
 import QuoteCard from "../QouteCard/QuoteCard";
+import styles from "./QuoteList.module.css"
 
 const QuoteList = ({ quotes }) => {
   return (
-    <div>
+    <div className={styles.quoteContainer}>
       {quotes.map((quote) => (
         <QuoteCard key={quote.id} quote={quote} />
       ))}
